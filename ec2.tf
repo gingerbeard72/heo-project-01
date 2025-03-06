4c4680d47b575ea0032cddc782a6edaa3b342625
gingerbeard72 <85934032+gingerbeard72@users.noreply.github.com>
2025-03-06 11:28:19 +1100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 # PostgreSQL DB Instance
 resource "aws_instance" "instance" {
-  ami                         = "ami-026eba2cb0d3e6856"
+  ami                         = "ami-"
   instance_type               = var.instance_type
-  iam_instance_profile        = "heo-Cartographer-EC2-Instance-Profile"
-  key_name                    = data.aws_key_pair.cartographer-db.public_key
+  iam_instance_profile        = "--EC2-Instance-Profile"
+  key_name                    = data.aws_key_pair.-db.public_key
   subnet_id                   = data.aws_subnet.compute-private-subnet-3.id
   associate_public_ip_address = false
   vpc_security_group_ids      = [data.aws_security_group.sec-group.id]
@@ -15,18 +15,18 @@ resource "aws_instance" "instance" {
     volume_type           = "gp3"
   }
   tags = {
-    Name                 = "Cartographer-db"
-    "heo:env:service"    = "prod:cartographer-db"
-    "QSConfigName-97uqz" = "heo-prod-patch-manager-default-policy"
+    Name                 = "-db"
+    ":env:service"    = "prod:-db"
+    "QSConfigName-97uqz" = "-prod-patch-manager-default-policy"
     "ami-backup"         = true
-    "heo:env:patch"      = "prod:true"
+    ":env:patch"      = "prod:true"
   }
   provider = aws.prod
 }
 
 
 # resource "aws_key_pair" "instance-key-pair" {
-#   key_name   = "cartographer-db"
-#   public_key = file("./cartographer-postgres.pub")
+#   key_name   = "-db"
+#   public_key = file("./-db.pub")
 #   provider   = aws.prod
 # }
