8cb3dffa96911cf302625caf66cd0ca1f55cc10f
gingerbeard72 <85934032+gingerbeard72@users.noreply.github.com>
2025-03-06 11:41:30 +1100

    Update sns.tf

--- sns.tf
+++ sns.tf
@@ -1,4 +1,4 @@
-resource "aws_sns_topic" "cartographer_errors" {
+resource "aws_sns_topic" "_errors" {
   display_name = "${var.ecs_name}-errors"
   name         = "${var.ecs_name}-errors"
   tags         = var.resource_tags
