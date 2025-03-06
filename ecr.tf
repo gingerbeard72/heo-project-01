76048224fdf5718a8aa127b3f882bec14b1d06aa
gingerbeard72 <85934032+gingerbeard72@users.noreply.github.com>
2025-03-06 11:29:02 +1100

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,7 @@
-# __generated__ by Terraform from "cartographer_health_monitoring_lambda_r"
-resource "aws_ecr_repository" "cartographer_health_monitoring_lambda_r" {
+# __generated__ by Terraform from "health_monitoring_lambda_r"
+resource "aws_ecr_repository" "health_monitoring_lambda_r" {
   image_tag_mutability = "MUTABLE"
-  name                 = "cartographer_health_monitoring_lambda_r"
+  name                 = "health_monitoring_lambda_r"
   tags                 = var.resource_tags
   encryption_configuration {
     encryption_type = "AES256"
@@ -11,18 +11,18 @@ resource "aws_ecr_repository" "cartographer_health_monitoring_lambda_r" {
   }
 }
 
-resource "aws_ecr_repository" "okapi_lambda" {
+resource "aws_ecr_repository" "lambda" {
   image_tag_mutability = "MUTABLE"
-  name                 = "cartographer_okapi_etl_lambda_r"
+  name                 = "etl_lambda_r"
   tags                 = var.resource_tags
   image_scanning_configuration {
     scan_on_push = false
   }
 }
 
-resource "aws_ecr_repository" "cartographer-external-ephemeris-etl-lambda" {
+resource "aws_ecr_repository" "external-ephemeris-etl-lambda" {
   image_tag_mutability = "MUTABLE"
-  name                 = "cartographer_external_ephemeris_etl_lambda_r"
+  name                 = "external_ephemeris_etl_lambda_r"
   tags                 = var.resource_tags
   image_scanning_configuration {
     scan_on_push = false
