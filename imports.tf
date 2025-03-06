754f0855507f90f82559a68ee89406ff8583e3a9
gingerbeard72 <85934032+gingerbeard72@users.noreply.github.com>
2025-03-06 11:35:55 +1100

    Update imports.tf

--- imports.tf
+++ imports.tf
@@ -1,84 +1,84 @@
 # import {
 #   to = aws_instance.instance
-#   id = "i-0c3d8fad07b436fac"
+#   id = ""
 # }
 
 # import {
-#   to = aws_ssm_parameter.cartographer-config
-#   id = "heo-cartographer-config"
+#   to = aws_ssm_parameter.config
+#   id = "config"
 # }
 
 # import {
-#   to = aws_cloudwatch_log_group.cartographer-health-monitor
-#   id = "/aws/lambda/cartographer-health-monitoring-lambda"
+#   to = aws_cloudwatch_log_group.health-monitor
+#   id = "/aws/lambda/health-monitoring-lambda"
 # }
 
 # import {
-#   to = aws_cloudwatch_log_group.cartographer-ipupdater
-#   id = "heo/cartographer_ip_updater"
+#   to = aws_cloudwatch_log_group.ipupdater
+#   id = "heo/ip_updater"
 # }
 
 # import {
 #   to = aws_cloudwatch_log_metric_filter.general_errors
-#   id = "/aws/lambda/cartographer-health-monitoring-lambda:General-Errors"
+#   id = "/aws/lambda/health-monitoring-lambda:General-Errors"
 # }
 
 # import {
 #   to = aws_cloudwatch_log_metric_filter.invalid_compressed_data
-#   id = "/aws/lambda/cartographer-health-monitoring-lambda:Invalid-Compressed-Data"
+#   id = "/aws/lambda/health-monitoring-lambda:Invalid-Compressed-Data"
 # }
 
 # import {
 #   to = aws_cloudwatch_log_metric_filter.omm_health_check_failed
-#   id = "/aws/lambda/cartographer-health-monitoring-lambda:OMM-Health-Check-Failed"
+#   id = "/aws/lambda/health-monitoring-lambda:OMM-Health-Check-Failed"
 # }
 
 # import {
 #   to = aws_cloudwatch_log_metric_filter.sp_health_check_failed
-#   id = "/aws/lambda/cartographer-health-monitoring-lambda:SP-Health-Check-Failed"
+#   id = "/aws/lambda/health-monitoring-lambda:SP-Health-Check-Failed"
 # }
 
 # import {
-#   to = aws_lambda_function.cartographer-health-monitoring-lambda
-#   id = "cartographer-health-monitoring-lambda"
+#   to = aws_lambda_function.health-monitoring-lambda
+#   id = "health-monitoring-lambda"
 # }
 
-# ## Okapi Imports - DS-368
+# ##  Imports - DS-368
 # import {
-#   to = aws_ecr_repository.okapi_lambda
-#   id = "cartographer_okapi_etl_lambda_r"
-# }
-
-# import {
-#   to = aws_cloudwatch_log_group.okapi-etl-lambda
-#   id = "/aws/lambda/cartographer-okapi-etl-lambda"
-# }
-# import {
-#   to = aws_iam_role.okapi-etl-lambda-role
-#   id = "cartographer-okapi-etl-lambda-role-uccmutv2"
+#   to = aws_ecr_repository._lambda
+#   id = "_etl_lambda_r"
 # }
 
 # import {
-#   to = aws_secretsmanager_secret.okapi-creds
-#   id = "arn:aws:secretsmanager:ap-southeast-2:416663440772:secret:heo_dev/cartographer-okapi-creds-DeQp6W"
+#   to = aws_cloudwatch_log_group.-etl-lambda
+#   id = "/aws/lambda/-etl-lambda"
+# }
+# import {
+#   to = aws_iam_role.-etl-lambda-role
+#   id = "-etl-lambda-role-"
 # }
 
 # import {
-#   to = aws_lambda_function.cartographer-okapi-etl-lambda
-#   id = "cartographer-okapi-etl-lambda"
+#   to = aws_secretsmanager_secret.-creds
+#   id = "arn:aws:secretsmanager:ap-southeast-2::secret:heo_dev/"
+# }
+
+# import {
+#   to = aws_lambda_function.-etl-lambda
+#   id = "-etl-lambda"
 # }
 
 import {
-  to = aws_lambda_function.cartographer-spacetrack-etl-lambda
-  id = "cartographer-spacetrack-etl-lambda"
+  to = aws_lambda_function.spacetrack-etl-lambda
+  id = "spacetrack-etl-lambda"
 }
 
 # import {
-#   to       = aws_lambda_function.cartographer-external-ephemeris-etl-lambda
-#   id       = "cartographer-external-ephemeris-etl-lambda"
+#   to       = aws_lambda_function.external-ephemeris-etl-lambda
+#   id       = "external-ephemeris-etl-lambda"
 # }
 
 # import {
-#   to       = aws_ssm_parameter.cartographer-external-ephemeris-etl-lambda-config
-#   id       = "cartographer-external-ephemeris-etl-lambda-config"
+#   to       = aws_ssm_parameter.external-ephemeris-etl-lambda-config
+#   id       = "external-ephemeris-etl-lambda-config"
 # }
