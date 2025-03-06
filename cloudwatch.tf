978a8d517e08ffef91672eabeae255e28d27cc70
gingerbeard72 <85934032+gingerbeard72@users.noreply.github.com>
2025-03-06 11:23:32 +1100

    Update cloudwatch.tf

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -7,10 +7,10 @@ resource "aws_cloudwatch_log_group" "log-group" {
   retention_in_days = 90
 }
 
-# __generated__ by Terraform from "heo/cartographer_ip_updater"
-resource "aws_cloudwatch_log_group" "cartographer-ipupdater" {
+# __generated__ by Terraform from "heo/_ip_updater"
+resource "aws_cloudwatch_log_group" "-ipupdater" {
   log_group_class   = "STANDARD"
-  name              = "heo/cartographer_ip_updater"
+  name              = "heo/_ip_updater"
   retention_in_days = 30
   skip_destroy      = false
   tags              = var.resource_tags
@@ -25,7 +25,7 @@ resource "aws_cloudwatch_metric_alarm" "ecs_service" {
   datapoints_to_alarm = 1
   dimensions = {
     ClusterName = "heo-inspect-cluster"
-    ServiceName = "cartographer"
+    ServiceName = ""
   }
   evaluation_periods = 1
   metric_name        = "CpuUtilized"
@@ -39,7 +39,7 @@ resource "aws_cloudwatch_metric_alarm" "ecs_service" {
 
 }
 
-resource "aws_cloudwatch_metric_alarm" "ecs_cluster_rocket" {
+resource "aws_cloudwatch_metric_alarm" "ecs_cluster_" {
   alarm_actions       = [data.aws_sns_topic.sns_topic.arn]
   alarm_description   = "This metric monitors ecs cpu utilization"
   alarm_name          = "ECS CPU 80% Utilization: ${var.container_name} cluster"
@@ -47,7 +47,7 @@ resource "aws_cloudwatch_metric_alarm" "ecs_cluster_rocket" {
   datapoints_to_alarm = 1
   dimensions = {
     ClusterName = "heo-inspect-cluster"
-    ServiceName = "cartographer"
+    ServiceName = ""
   }
   evaluation_periods        = 1
   insufficient_data_actions = []
@@ -65,7 +65,7 @@ resource "aws_cloudwatch_metric_alarm" "ecs_cluster_rocket" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "ec2_95_cpu_utilization_alarm" {
-  alarm_name                = "CPU 95% Utilization: Cartographer DB"
+  alarm_name                = "CPU 95% Utilization:  DB"
   alarm_actions             = [data.aws_sns_topic.sns_topic.arn]
   actions_enabled           = true
   comparison_operator       = "GreaterThanThreshold"
@@ -77,14 +77,14 @@ resource "aws_cloudwatch_metric_alarm" "ec2_95_cpu_utilization_alarm" {
   period                    = 300
   statistic                 = "Average"
   threshold                 = 95
-  alarm_description         = "This metric monitors the CPU utilization for the Cartographer DB Instance"
+  alarm_description         = "This metric monitors the CPU utilization for the  DB Instance"
   insufficient_data_actions = []
 
   provider = aws.prod
 }
 
 
-#Cartographer Health Monitor Alarms
+# Health Monitor Alarms
 
 resource "aws_cloudwatch_log_metric_filter" "metric_filters" {
   for_each       = var.health_monitor_objects
@@ -93,14 +93,14 @@ resource "aws_cloudwatch_log_metric_filter" "metric_filters" {
   pattern        = each.value.pattern
   metric_transformation {
     name      = each.value.name
-    namespace = "cartographer"
+    namespace = ""
     value     = "1"
   }
   provider = aws.prod
 }
 
-# __generated__ by Terraform from "/aws/lambda/cartographer-health-monitoring-lambda"
-resource "aws_cloudwatch_log_group" "cartographer-health-monitor" {
+# __generated__ by Terraform from "/aws/lambda/-health-monitoring-lambda"
+resource "aws_cloudwatch_log_group" "-health-monitor" {
   log_group_class   = "STANDARD"
   name              = "/aws/lambda/${var.health_monitor_lambda}"
   retention_in_days = 0
@@ -113,7 +113,7 @@ resource "aws_cloudwatch_metric_alarm" "metric_filter_alarms" {
   for_each = var.health_monitors
   alarm_actions = concat(
     each.value.alarm_actions,
-    [aws_sns_topic.cartographer_errors.arn]
+    [aws_sns_topic._errors.arn]
   )
   alarm_description   = "${var.ecs_name} ${each.value.name} alert"
   alarm_name          = "${var.ecs_name} ${each.value.name} alert"
@@ -131,39 +131,39 @@ resource "aws_cloudwatch_metric_alarm" "metric_filter_alarms" {
 }
 
 
-## Okapi resources
+##  resources
 
-resource "aws_cloudwatch_log_group" "okapi-etl-lambda" {
+resource "aws_cloudwatch_log_group" "-etl-lambda" {
   log_group_class   = "STANDARD"
-  name              = "/aws/lambda/cartographer-okapi-etl-lambda"
+  name              = "/aws/lambda/--etl-lambda"
   retention_in_days = 180
   skip_destroy      = false
   tags              = var.resource_tags
   provider          = aws.prod
 }
 
-resource "aws_cloudwatch_log_metric_filter" "okapi_metric_filters" {
-  log_group_name = aws_cloudwatch_log_group.okapi-etl-lambda.name
+resource "aws_cloudwatch_log_metric_filter" "_metric_filters" {
+  log_group_name = aws_cloudwatch_log_group.-etl-lambda.name
   name           = "General-Errors"
   pattern        = var.cw_metric_filter_pattern
   metric_transformation {
-    name      = "cartographer"
-    namespace = var.cartographer-okapi-etl-lambda
+    name      = ""
+    namespace = var.--etl-lambda
     value     = "1"
   }
   provider = aws.prod
 }
 
-resource "aws_cloudwatch_metric_alarm" "okapi_metric_filter_alarms" {
-  alarm_actions       = [aws_sns_topic.cartographer_errors.arn]
-  alarm_description   = "${var.cartographer-okapi-etl-lambda} Alert"
-  alarm_name          = "${var.cartographer-okapi-etl-lambda} alert"
+resource "aws_cloudwatch_metric_alarm" "_metric_filter_alarms" {
+  alarm_actions       = [aws_sns_topic._errors.arn]
+  alarm_description   = "${var.--etl-lambda} Alert"
+  alarm_name          = "${var.--etl-lambda} alert"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
-  #metric_name         = lookup(aws_cloudwatch_log_metric_filter.okapi_metric_filters.metric_transformation, "name")
-  metric_name = aws_cloudwatch_log_metric_filter.okapi_metric_filters.metric_transformation[0].name
+  #metric_name         = lookup(aws_cloudwatch_log_metric_filter._metric_filters.metric_transformation, "name")
+  metric_name = aws_cloudwatch_log_metric_filter._metric_filters.metric_transformation[0].name
   #metric_name        = var.ecs_name
-  namespace          = var.cartographer-okapi-etl-lambda
+  namespace          = var.--etl-lambda
   statistic          = "Sum"
   period             = "60"
   threshold          = 1
@@ -174,25 +174,25 @@ resource "aws_cloudwatch_metric_alarm" "okapi_metric_filter_alarms" {
 
 
 resource "aws_cloudwatch_log_metric_filter" "ipupdater_metric_filters" {
-  log_group_name = "heo/cartographer_ip_updater"
+  log_group_name = "heo/_ip_updater"
   name           = "General Errors"
   pattern        = var.cw_metric_filter_pattern
   metric_transformation {
-    name      = "cartographer"
-    namespace = "Cartographer_IP_Updater"
+    name      = ""
+    namespace = "_IP_Updater"
     value     = "1"
   }
   provider = aws.prod
 }
 
 resource "aws_cloudwatch_metric_alarm" "ipupdater_metric_filter_alarms" {
-  alarm_actions       = [aws_sns_topic.cartographer_errors.arn]
-  alarm_description   = "Cartographer IP Updater Alert"
-  alarm_name          = "Cartographer IP Updater alert"
+  alarm_actions       = [aws_sns_topic._errors.arn]
+  alarm_description   = " IP Updater Alert"
+  alarm_name          = " IP Updater alert"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
   metric_name         = aws_cloudwatch_log_metric_filter.ipupdater_metric_filters.metric_transformation[0].name
-  namespace           = "Cartographer_IP_Updater"
+  namespace           = "_IP_Updater"
   statistic           = "Sum"
   period              = "60"
   threshold           = 0
@@ -201,26 +201,26 @@ resource "aws_cloudwatch_metric_alarm" "ipupdater_metric_filter_alarms" {
   provider            = aws.prod
 }
 
-resource "aws_cloudwatch_log_metric_filter" "cartographer_fatal_error_filters" {
-  log_group_name = "/ecs/cartographer-prod"
+resource "aws_cloudwatch_log_metric_filter" "_fatal_error_filters" {
+  log_group_name = "/ecs/-prod"
   name           = "FATAL Errors"
   pattern        = "FATAL"
   metric_transformation {
-    name      = "Cartographer_Fatal_Error"
-    namespace = "cartographer"
+    name      = "_Fatal_Error"
+    namespace = ""
     value     = "1"
   }
   provider = aws.prod
 }
 
-resource "aws_cloudwatch_metric_alarm" "cartographer_fatal_error_filter_alarms" {
-  alarm_actions       = [aws_sns_topic.cartographer_errors.arn]
-  alarm_description   = "Cartographer Fatal Error Alert"
-  alarm_name          = "Cartographer Fatal Error alert"
+resource "aws_cloudwatch_metric_alarm" "_fatal_error_filter_alarms" {
+  alarm_actions       = [aws_sns_topic._errors.arn]
+  alarm_description   = " Fatal Error Alert"
+  alarm_name          = " Fatal Error alert"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
-  metric_name         = aws_cloudwatch_log_metric_filter.cartographer_fatal_error_filters.metric_transformation[0].name
-  namespace           = "Cartographer_Fatal_Error"
+  metric_name         = aws_cloudwatch_log_metric_filter._fatal_error_filters.metric_transformation[0].name
+  namespace           = "_Fatal_Error"
   statistic           = "Sum"
   period              = "60"
   threshold           = 0
@@ -229,35 +229,35 @@ resource "aws_cloudwatch_metric_alarm" "cartographer_fatal_error_filter_alarms" 
   provider            = aws.prod
 }
 
-resource "aws_cloudwatch_log_group" "cartographer-external-ephemeris-etl-lambda" {
+resource "aws_cloudwatch_log_group" "-external-ephemeris-etl-lambda" {
   log_group_class   = "STANDARD"
-  name              = "/aws/lambda/cartographer-external-ephemeris-etl-lambda"
+  name              = "/aws/lambda/-external-ephemeris-etl-lambda"
   retention_in_days = 180
   skip_destroy      = false
   tags              = var.resource_tags
   provider          = aws.prod
 }
 
-resource "aws_cloudwatch_log_metric_filter" "cartographer_queuepool_metric_filter" {
+resource "aws_cloudwatch_log_metric_filter" "_queuepool_metric_filter" {
   log_group_name = aws_cloudwatch_log_group.log-group.name
-  name           = "Cartographer DB QueuePool limit"
+  name           = " DB QueuePool limit"
   pattern        = "TimeoutError QueuePool limit"
   metric_transformation {
-    name      = "Cartographer_QueuePool_Limit"
-    namespace = "cartographer"
+    name      = "_QueuePool_Limit"
+    namespace = ""
     value     = "1"
   }
   provider = aws.prod
 }
 
-resource "aws_cloudwatch_metric_alarm" "cartographer_queuepool_metric_filter_alarms" {
-  alarm_actions       = [aws_sns_topic.cartographer_errors.arn]
-  alarm_description   = "Cartographer QueuePool limit Alert"
-  alarm_name          = "Cartographer QueuePool limit alert"
+resource "aws_cloudwatch_metric_alarm" "_queuepool_metric_filter_alarms" {
+  alarm_actions       = [aws_sns_topic._errors.arn]
+  alarm_description   = " QueuePool limit Alert"
+  alarm_name          = " QueuePool limit alert"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
-  metric_name         = aws_cloudwatch_log_metric_filter.cartographer_queuepool_metric_filter.metric_transformation[0].name
-  namespace           = "cartographer"
+  metric_name         = aws_cloudwatch_log_metric_filter._queuepool_metric_filter.metric_transformation[0].name
+  namespace           = ""
   statistic           = "Sum"
   period              = "60"
   threshold           = 1
